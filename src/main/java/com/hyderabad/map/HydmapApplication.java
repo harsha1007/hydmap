f7b5b97d1cdd325a2830b826ba593f21dff4cc33
harsha1007 <harsha.kunapareddy@gmail.com>
2017-03-13 23:40:08 -0700

    Initial commit for HydMap

---
+++ src/main/java/com/hyderabad/map/HydmapApplication.java
@@ -0,0 +1,12 @@
+package com.hyderabad.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydmapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hyderabad/map/HydmapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hyderabad.map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HydmapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
